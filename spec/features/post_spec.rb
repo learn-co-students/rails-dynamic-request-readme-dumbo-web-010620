13abf1e48998b912e8b89416df0ac8cda4713e34
github username <github email address>
2020-01-31 11:10:55 -0500

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,7 +11,7 @@ describe 'navigate' do
   end
 
   it 'shows the title on the show page in an h1 tag' do
-    visit "/posts/#{@post.id}"
+    visit"/posts/#{@post.id}"
     expect(page).to have_css("h1", text: "My Post")
   end
 
